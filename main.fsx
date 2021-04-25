bc78aa8eb5ebb74021a2d374b01e7321b356614d
andreasgarvik <andreas.garvik13@gmail.com>
2021-04-25 16:13:59 +0200

    :sparkles:

--- main.fsx
+++
@@ -1,6 +0,0 @@
-type multiset<'a> = list<int * 'a>
-
-let (^) a b = [ (a, b) ]
-let (+) ams bms = List.append ams bms
-
-let ms : multiset<string> = (1 ^ "hello") + (1 ^ "cpn")
---
+++ src/simple.fs
@@ -0,0 +1,12 @@
+// First need a way to represent the model
+// Code should be in the CPN module/file
+// Should be able to represent all possible cpn models
+
+// Second need a way to represent the state of the model
+// Code should be in a State module/file
+
+// Third need a way to advance or reverse the state
+// Code should be in the State moduleY/file??
+
+// Possible ways to represent state
+// 1. 
